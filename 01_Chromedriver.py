29d6366f11388788908f0a0ce34359188605fe67
tatiana <khen.tatiana@gmail.com>
2020-03-14 21:38:31 -0700

    initiate "chromedriver"

--- 01_Chromedriver.py
+++ 01_Chromedriver.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 
 def browser():
-	browser = webdriver.Chrome(executable_path="")
+	link = "http://selenium1py.pythonanywhere.com"
+	browser = webdriver.Chrome(executable_path="/Users/tatiana/Desktop/PROJECTS/chromedriver")
 	browser.get(link)
+	print(browser.title)
