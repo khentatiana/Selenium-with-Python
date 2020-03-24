e2f60f433ac8441e02abdf033ef39e4c95931a61
tatiana <khen.tatiana@gmail.com>
2020-03-24 15:05:27 -0700

    39 thru 41 - Pytest

--- test_pytest_41_run_multiple_tests/test_Signup.py
+++ test_pytest_41_run_multiple_tests/test_Signup.py
@@ -11,8 +11,10 @@ def setup():
 def test_SignupByEmail(setup):
     print("\nThis is signup by email test")
 
+
 def test_SignupByFacebook(setup):
     print("\nThis is signup by Facebook test")
+
 
 def test_SignupByTwitter(setup):
     print("\nThis is signup by Twitter test")
